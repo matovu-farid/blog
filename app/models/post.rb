7c3e8ab528eee2170002e1ee0133aa5ce50ed240
Farid <matovu90@gmail.com>
2022-05-03 13:10:33 +0300

    Add user and post method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,13 @@
 class Post < ApplicationRecord
-  belongs_to :author,class_name: 'User', counter_cache: :posts_counter
+  belongs_to :author,class_name: 'User'
   has_many :likes
   has_many :comments
+  def update_posts_count(value)
+   user = User.where('id = ?',self.author_id).first
+   user.posts_counter = value
+   user.save
+  end
+  def recent_comments 
+   Comment.where('post_id = ?',self.id).last(5)
+  end
 end
