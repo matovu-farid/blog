d0f4231f51b06b536a7f52ff5efd0d5b32fc9557
Farid <matovu90@gmail.com>
2022-05-04 18:52:33 +0300

    Add post page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,11 @@
 class PostsController < ApplicationController
   def index
     @user = current_user
-    @posts = current_user.posts
+    @posts = current_user.last_three_posts
   end
 
-  def show; end
+  def show
+      @post = Post.find params[:id]
+      @comments = @post.recent_comments
+  end
 end
