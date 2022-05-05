00ec7e6f365067609f9eaffa844cf51290ac5735
Farid <matovu90@gmail.com>
2022-05-05 16:33:56 +0300

    Add the post#new form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,20 @@ class PostsController < ApplicationController
     @post = Post.find params[:id]
     @comments = @post.comments
   end
+  def new
+    p 'New hit'
+     render :new
+  end
+  def create
+    @post = Post.new(params.require(:post).permit(:title,:text))
+    @post.author_id = params[:user_id]
+    if @post.save
+      flash[:success]='Your post was created Successfully'
+      redirect_to user_posts_url
+    else
+      p "Not saved"
+      p @post.errors.full_messages
+      render :new
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  validates :title,presence: true
+  validates :text,presence:true
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
