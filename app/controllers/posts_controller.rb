33ed74a6b1c3c6a45a259462278a1a779c19b971
Farid <matovu90@gmail.com>
2022-05-05 17:24:21 +0300

    Add edit#post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,23 +8,45 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find params[:id]
+    @post = find_post
     @comments = @post.comments
   end
   def new
-    p 'New hit'
-     render :new
+     
+  end
+  def edit
+    @post = find_post
   end
   def create
-    @post = Post.new(params.require(:post).permit(:title,:text))
+    @post = new_post
     @post.author_id = params[:user_id]
     if @post.save
       flash[:success]='Your post was created Successfully'
-      redirect_to user_posts_url
+      redirect_to user_post_url(id:@post.id)
     else
-      p "Not saved"
-      p @post.errors.full_messages
       render :new
     end
+  end
+  def update
+    new_post = :new_post
+    saved_post = find_post
+    saved_post.title = new_post.title
+    save_post.text = new_post.text
+
+    if save_post.save
+      flash[:success]='Your post was created Successfully'
+      redirect_to user_post_url(id:@post.id)
+    else
+      @post = saved_post
+      flash.now[:alert] = 'Invalid Input'
+      render :edit
+    end
+  end
+  private
+  def new_post
+    Post.new(params.require(:post).permit(:title,:text))
+  end
+  def find_post
+    Post.find params[:id]
   end
 end
