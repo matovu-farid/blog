f12bf6a906fbdaa73f264b2ed678431f65cfd129
matovu-farid <matovu90@gmail.com>
2022-05-09 19:51:15 +0300

    Add login bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,20 +2,11 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  def create
  
-    user = User.new params
-    user.save
-  end
-  def update
-    user = User.new params
-    user.save
-  end
+ 
   def show
+    
     @user = User.find(params[:id])
   end
-  private
-  def user_params
-     params.require(:user).permit(:name,:password,:password_confirmation)
-  end
+  
 end
