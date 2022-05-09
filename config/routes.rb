877d00ee55a5a18d10141e225c29f0ffdfb66de7
matovu-farid <matovu90@gmail.com>
2022-05-09 21:57:37 +0300

    Configure logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'users#index'
-  resources :users, only: [:index,:show,:create,:update] do
+  resources :users, only: [:index,:show] do
     resources :posts, only: [:index, :show,:new,:create,:edit,:update] do
       resources :comments, only: [:new,:create,:edit,:update]
       resources :likes, only:[:create]
