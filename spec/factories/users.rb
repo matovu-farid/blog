f39cadb4b8d86edaa78c0a0f07361c03cc467053
Adorn Choga <adornchoga@gmail.com>
2022-05-11 16:06:05 +0200

    Add specs for login page

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,12 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
+    name { "Farid" }
     photo { "MyString" }
     bio { "MyString" }
-    email { "MyString" }
+    sequence(:email) {|n| "farid#{n}@example.com" }
+    password { 'password' }
+    password_confirmation { 'password' }
+    confirmed_at { DateTime.now }
     role { "user" }
     factory :admin do
       role { "admin" }
