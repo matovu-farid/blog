2e8ef777a009fb9b2dfd239b0a38e2a6ababe7a1
matovu-farid <matovu90@gmail.com>
2022-05-12 01:25:39 +0300

    Add user index check for fields

--- spec/system/user_post_indices_spec.rb
+++ spec/system/user_post_indices_spec.rb
@@ -5,5 +5,40 @@ RSpec.describe "UserPostIndices", type: :system do
     driven_by(:rack_test)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  before do
+    driven_by(:rack_test)
+  end
+  before(:each) do 
+    @user = create(:user,name: "current_user")
+    @visited = create(:user,name: "visited_user")
+    sign_in @user
+  end
+  
+ 
+    it "should be able to see the user's profile picture" do
+      visit user_posts_url(@visited)
+      expect(page).to have_css("img[src*='#{@visited.photo}']")
+    end
+    it "should be able to see the user's name" do
+      visit user_posts_url(@visited)
+      expect(page).to have_content(@visited.name)
+    end
+    context "For Post" do
+      before(:each){@post = create(:post, author: @visited,title:'title')}
+      it "should be able to see the number of posts the user has written" do
+        visit user_posts_url(@visited)
+        expect(page).to have_content("Number of posts: 1")
+      end
+      it "should be able to see a posts title" do
+        visit user_posts_url(@visited)
+        expect(page).to have_content('title')
+      end
+  end  
+  # it "should be able to redirect to a users show page" do
+  
+  #  visit root_url
+     
+  #   page.first("a[href*='#{user_path(@user)}']").click
+  #   expect(current_url).to eq(user_url(@user))
+  # end
 end
